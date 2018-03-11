83f1d021fa66edf7e7058887ead36ea15736e928
Stephen Poole <stephenmpoole@gmail.com>
2018-03-11 10:17:55 +0000

    Main residence relief working

--- test/taxlogic_test.dart
+++ test/taxlogic_test.dart
@@ -2,11 +2,7 @@ import 'package:taxlogic/taxlogic.dart';
 import 'package:test/test.dart';
 
 
-num fix(num input){
 
-  return num.parse(input.toStringAsFixed(2));
-
-}
 
 void main() {
 
@@ -351,25 +347,25 @@ void incomeTaxEngland2017(){
     test('20,000 2017 england', () {
       taxPosition.earnings = 20000;
       incomeTaxPosition.calculate();
-      expect(fix(incomeTaxPosition.tax), 1800);
+      expect(incomeTaxPosition.tax, 1800);
     });
 
     test('45,000 2017 england', () {
       taxPosition.earnings = 45000;
       taxPosition.incomeTax.calculate();
-      expect(fix(taxPosition.incomeTax.tax), 7200);
+      expect(taxPosition.incomeTax.tax, 7200);
     });
 
     test('90,000 2017 england', () {
       taxPosition.earnings = 90000;
       taxPosition.incomeTax.calculate();
-      expect(fix(taxPosition.incomeTax.tax), 25200);
+      expect(taxPosition.incomeTax.tax, 25200);
     });
 
     test('200,000 2017 england', () {
       taxPosition.earnings = 200000;
       taxPosition.incomeTax.calculate();
-      expect(fix(taxPosition.incomeTax.tax), 76100);
+      expect(taxPosition.incomeTax.tax, 76100);
     });
 
 
@@ -709,52 +705,52 @@ void nationalInsuranceEarnings() {
     test('5,000 2018 employee', () {
       taxPosition.earnings = 5000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass1p), 0);
+      expect(nicPosition.nicClass1p, 0);
     });
 
     test('5,000 2018 employer', () {
       taxPosition.earnings = 5000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass1s), 0);
+      expect(nicPosition.nicClass1s, 0);
     });
 
     test('20,000 2018 employee', () {
       taxPosition.earnings = 20000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass1p), 1420.8);
+      expect(nicPosition.nicClass1p, 1420.8);
 
     });
 
     test('20,000 2018 employer', () {
       taxPosition.earnings = 20000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass1s), 1633.92);
+      expect(nicPosition.nicClass1s, 1633.92);
     });
 
     test('45,000 2018 employee', () {
       taxPosition.earnings = 45000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass1p), 4420.8);
+      expect(nicPosition.nicClass1p, 4420.8);
 
     });
 
     test('45,000 2018 employer', () {
       taxPosition.earnings = 45000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass1s), 5083.92);
+      expect(nicPosition.nicClass1s, 5083.92);
     });
 
     test('90,000 2018 employee', () {
       taxPosition.earnings = 90000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass1p), 5320.8);
+      expect(nicPosition.nicClass1p, 5320.8);
 
     });
 
     test('90,000 2018 employer', () {
       taxPosition.earnings = 90000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass1s), 11293.92);
+      expect(nicPosition.nicClass1s, 11293.92);
     });
 
 
@@ -780,13 +776,13 @@ void nationalInsuranceTrade() {
     test('5,000 2018 trade', () {
       taxPosition.trade = 5000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass4), 0);
+      expect(nicPosition.nicClass4, 0);
     });
 
     test('20,000 2018 trade', () {
       taxPosition.trade = 20000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass4), 1065.24);
+      expect(nicPosition.nicClass4, 1065.24);
 
     });
 
@@ -794,14 +790,14 @@ void nationalInsuranceTrade() {
     test('45,000 2018 trade', () {
       taxPosition.trade = 45000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass4), 3315.24);
+      expect(nicPosition.nicClass4, 3315.24);
 
     });
 
     test('90,000 2018 trade', () {
       taxPosition.trade = 90000;
       nicPosition.calculate();
-      expect(fix(nicPosition.nicClass4), 4215.24);
+      expect(nicPosition.nicClass4, 4215.24);
 
     });
 
