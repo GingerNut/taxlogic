669609bc2118355db31dc495c26801a7a769ee0b
Stephen Poole <stephenmpoole@gmail.com>
2018-03-13 20:11:02 +0000

    starting accounts

--- lib/src/accounts/accounts.dart
+++ lib/src/accounts/accounts.dart
@@ -1,10 +1,20 @@
-
+import '../date.dart';
 
 
 
 class Accounts{
 
+  List<Account> accounts = new List();
 
+
+
+  List<Account> get chart {
+    List<Account> chart = new List();
+
+    chart.add(new Account(01100, 'Cost'));
+
+    return chart;
+  }
 
 
 }
@@ -12,5 +22,40 @@ class Accounts{
 
 class Account{
 
+  int number;
+  String name;
+  num balance;
+
+  Account(this.number, this.name);
+
+}
+
+class Entry{
+  Date date;
+  Debit debit;
+  Credit credit;
+
+
+}
+
+class Journal{
+  Date date;
+  Debit debit;
+  Credit credit;
+  String narrative;
+
+}
+
+class Debit{
+  Account account;
+  num amount;
+
+
+
+}
+
+class Credit{
+  Account account;
+  num amount;
 
 }
---
+++ lib/src/accounts/income_and_expenditure.dart
@@ -0,0 +1,31 @@
+
+
+
+
+
+class IncomeAndExpenditure{
+
+  List<Income> income = new List();
+  List<Expenditure> expenditure = new List();
+
+
+
+
+
+}
+
+class Income{
+  String name;
+
+
+
+}
+
+
+class Expenditure{
+  String name;
+  bool finance = false;
+
+
+
+}
